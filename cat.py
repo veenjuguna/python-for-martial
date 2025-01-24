da81688643cc59c5ca9585d279e514082eac5443
veenjuguna <167479023+veenjuguna@users.noreply.github.com>
2025-01-24 21:46:32 +0300

    cat

---
+++ cat.py
@@ -0,0 +1,21 @@
+import time
+import os
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.position = 0
+
+    def walk(self):
+        while True:
+            self.position += 1
+            self.display()
+            time.sleep(0.5)
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+    def display(self):
+        print(" " * self.position + "🐱")
+
+if __name__ == "__main__":
+    my_cat = Cat("Whiskers")
+    my_cat.walk()
