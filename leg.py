72049cc268d2402d2dcc27f77098fe2527e1fe97
veenjuguna <167479023+veenjuguna@users.noreply.github.com>
2025-02-11 22:15:08 +0300

    heyey

--- leg.py
+++ leg.py
@@ -5,3 +5,8 @@ if __name__ == "__main__":
     input_string = "martialarts"
     sorted_string = sort_alphabets(input_string)
     print(f"Sorted alphabets: {sorted_string}")
+    # /sxghalmz,xgcujnds
+    # wstuajh iwed
+    # wqduinas
+    # saxjx xaukqsa\
+    # dqh acjud jduhndnuhbhed xnxu9nx va sab ajshbwu sadij0q ej
